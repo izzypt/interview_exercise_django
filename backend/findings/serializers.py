273a30fd5b91a7518c948757a86535be0bf282bd
izzypt <bonvalots@gmail.com>
2023-03-07 02:35:11 +0000

    added 1 test for the view

--- backend/findings/serializers.py
+++ backend/findings/serializers.py
@@ -5,16 +5,15 @@ class ScanSerializer(serializers.ModelSerializer):
     class Meta:
         model = ScanModel
         fields = ('value',)
-    
+
     def to_representation(self, instance):
         # ModelSerializer serializes a model instance to a dictionary-like object, we need to override that behavior because we want to return a string for each scan.
-        # to_representation allows to define how the object should be serialized and what data should be included in the serialized representation.
+        # to_representation allows to define how the object should be serialized and what data should be included.
         return instance.value
 
 class FindingsSerializer(serializers.ModelSerializer):
     scans = ScanSerializer(many=True)
-    
+
     class Meta:
         model = FindingsModel
         fields = ('id', 'definition_id', 'target_id', 'url', 'path', 'method', 'scans')
-        
--- backend/findings/tests.py
+++ backend/findings/tests.py
@@ -1,3 +1,37 @@
 from django.test import TestCase
+from django.test import Client
+from django.urls import reverse #reverse() is used to generate a URL pattern for a given view function or class (provide the name).
+from .models import FindingsModel, ScanModel
 
-# Create your tests here.
+class testFindingsList(TestCase):
+    # setUp method is executed before each test method runs
+    def setUp(self):
+        self.url = reverse('findings_list')
+        # Client() provides a way to simulate HTTP requests to your Django application. 
+        # You can use it to test your views and ensure that they return the expected responses.
+        self.client = Client()
+        
+    def test_findings_list_return_404(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 404)
+        
+    def test_findings_list_return_200(self):
+        # Create a new ScanModel instance
+        scan = ScanModel.objects.create(value='example scan value')
+        # Create a new FindingsModel instance and add the scan to its scans many-to-many field
+        findings = FindingsModel.objects.create(
+            target_id='example target id',
+            definition_id='example definition id',
+            url='http://example.com',
+            path='/example/path',
+            method='GET'
+        )
+        findings.scans.add(scan)
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        
+        
+# Django creates a new database for testing and runs tests within that database. 
+# Each test method in Django is atomic and isolated from the others, meaning that the database state is reset before each test method is executed. 
+# This is done to ensure that tests run independently of each other and that there are no side effects or interference between tests. 
+# Except setUp method which is executed before each test method and is used to set up any necessary data or objects that are required for the tests.
